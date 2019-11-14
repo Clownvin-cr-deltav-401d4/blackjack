b605861ef1992c84f69e7aa81c533e80ed2f5d9d
Calvin G Hall <123chall@gmail.com>
2019-11-14 01:14:28 -0600

    Improved dealer difficulty

--- app/app.js
+++ app/app.js
@@ -6,11 +6,11 @@ function totalHand(hand) {
   return hand.reduce((total, card) => total + card.value, 0);
 }
 
-function shouldPlay(handValue, deck, tolerance = 0.5) {
+function shouldPlay(opponentValue, handValue, deck, tolerance = 0.5) {
   const cutoff = 21 - handValue;
   const goodDraws = deck.cards.filter((card) => card.value <= cutoff);
   console.log('HERE'+((goodDraws.length / deck.cards.length) > tolerance));
-  return (goodDraws.length / deck.cards.length) > tolerance;
+  return opponentValue > handValue && (goodDraws.length / deck.cards.length) > tolerance;
 }
 
 blackjack.controller('BlackjackController', ['$scope', function (scope) {
@@ -23,29 +23,33 @@ blackjack.controller('BlackjackController', ['$scope', function (scope) {
     scope.hand.push(scope.deck.draw());
     scope.handValue = totalHand(scope.hand);
     if (scope.handValue > 21) {
-      scope.victory = false;
-      return scope.end();
+      return scope.end(false);
     }
-    if (shouldPlay(scope.dealerValue, scope.deck)) {
-      scope.dealerHand.push(scope.deck.draw());
-      scope.dealerValue = totalHand(scope.dealerHand);
-      if (scope.dealerValue > 21) {
-        scope.victory = true;
-        scope.end();
-      }
-    }
+    scope.dealerDraw();
   };
+  scope.dealerDraw = () => {
+    if (!shouldPlay(scope.handValue, scope.dealerValue, scope.deck)) return;
+    scope.dealerHand.push(scope.deck.draw());
+    scope.dealerValue = totalHand(scope.dealerHand);
+    if (scope.dealerValue > 21) {
+      scope.end(true);
+    }
+  }
   scope.reset = () => {
     scope.deck.putBack(...scope.hand.splice(0));
     scope.deck.putBack(...scope.dealerHand.splice(0));
     scope.deck.shuffle();
     scope.handleValue = 0;
     scope.dealerValue = 0;
+    delete scope.victory;
     scope.draw();
     scope.endscreenClass = 'not-visible';
   }
-  scope.end = () => {
-    if (scope.victory || scope.handValue <= 21 && scope.handValue >= scope.dealerValue) {
+  scope.end = (victory) => {
+    while ((victory || typeof victory === 'undefined') && shouldPlay(scope.handValue, scope.dealerValue, scope.deck)) scope.dealerDraw();
+    if (typeof victory !== 'undefined' || typeof scope.victory !== 'undefined') {
+      scope.victory = victory || scope.victory;
+    } else if (scope.handValue <= 21 && scope.handValue >= scope.dealerValue) {
       scope.victory = true;
     } else {
       scope.victory = false;
@@ -54,7 +58,6 @@ blackjack.controller('BlackjackController', ['$scope', function (scope) {
     scope.endscreenClass = 'visible';
   }
   scope.draw();
-  scope.victory = false;
   scope.endscreenClass = 'not-visible';
 }]);
 
